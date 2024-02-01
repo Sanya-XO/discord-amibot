0afb3186155e069f161364b6043a449f5a24a8cf
SanyaXO <sanya.xo1337@gmail.com>
2024-02-01 21:58:53 +0530

    fix(bot): fixed result formatting

--- bot/src/commands/result.rs
+++ bot/src/commands/result.rs
@@ -1,10 +1,8 @@
-use std::ops::Deref;
-
-use amizone::api::{client::UserClient, types::{ExamResultRecord, OverallResult, Course, Semester, go_amizone::server::proto::v1::{Score, Credits}}};
-use poise::serenity_prelude::CreateEmbed;
+use amizone::api::client::UserClient;
+use amizone::api::types::{ExamResultRecord, OverallResult, go_amizone::server::proto::v1::{Score, Credits}};
 
 use crate::{CommandResult, Context, Result};
-use crate::util;
+
 use log::info;
 
 static RESULT_HELP: &str = "/results - Retrieve your examination results.\n\n\
@@ -21,31 +19,55 @@ pub async fn results(ctx: Context<'_>,
     #[description = "Semester number"] semester: Option<usize>) -> CommandResult {
     
     ctx.defer().await?;
+    
     let mut invocation_data = ctx.invocation_data::<Result<UserClient>>().await.unwrap();
 
     let client = invocation_data.as_mut()?;
 
-    //let (course_result, overall_result) = client.get_current_exam_result().await?;
-    let (course_result, overall_result) = match semester {
+    let (course_results, overall_result) = match semester {
         Some(sem) => client.get_exam_result(sem).await?,
         None => client.get_current_exam_result().await?,
     };
 
-    let course_wise = course_result.into_iter()
-    .map(|course| course.into())
-    .collect::<Vec<ExamResultRecord>>();
-    
-    let course_names = course_wise
-        .iter()
-        .map(|course| match course.course{
-            Some(ref course_ref) => course_ref.name.as_str(),
-            None => "",
-        })
-        .collect::<Vec<&str>>();
-
-    let overall_semester_wise = overall_result.into_iter()
+    let overall_result_vec = overall_result.into_iter()
         .map(|semester| semester.into())
         .collect::<Vec<OverallResult>>();
+
+    let mut message = format!("**Semester Result**\n\n```");
+    
+    for sem_result in overall_result_vec {
+        
+        let sem_num = match sem_result.semester {
+            Some(ref sem_ref) => sem_ref.semester_ref.as_str(),
+            None => ""
+        };
+
+        let sgpa = sem_result.semester_grade_point_average;
+
+        let cgpa = sem_result.cumulative_grade_point_average;
+
+        message.push_str(&format!("Semester: {}\n", sem_num));
+        message.push_str(&format!("SGPA: {}\n", &sgpa));
+        message.push_str(&format!("CGPA: {}\n\n", &cgpa));
+    }
+    message.push_str("```");
+    
+    ctx.say(message).await?;
+    Ok(())
+    
+    // let course_wise = course_result.into_iter()
+    // .map(|course| course.into())
+    // .collect::<Vec<ExamResultRecord>>();
+    
+    // let course_names = course_wise
+    //     .iter()
+    //     .map(|course| match course.course{
+    //         Some(ref course_ref) => course_ref.name.as_str(),
+    //         None => "",
+    //     })
+    //     .collect::<Vec<&str>>();
+
+
 
 
 
@@ -53,12 +75,10 @@ pub async fn results(ctx: Context<'_>,
     //     .iter()
     //     .map(|course| match course.score{
     //         Some(ref score) => (score.max, score.grade.as_str(), score.grade_point),
-    //         None => (0, "nil", 0)
+    //         None => (-1, "", -1)
     //     })
     //     .collect::<Vec<(i32, &str, i32)>>();
-    
 
-    Ok(())
     
 }
 fn result_help() -> String {
